88865eb5cf07349938ce532f58bfe3558e264518
Ahmad Aslam Bin Ahmad Nasir <qrazier@gmail.com>
2017-04-10 03:01:53 +0800

    Create str2bin.c

---
+++ C/str2bin.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* stringToBinary(char* s) {
+    if(s == NULL) return 0; /* no input string */
+    size_t len = strlen(s);
+    char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
+    binary[0] = '\0';
+    for(size_t i = 0; i < len; ++i) {
+        char ch = s[i];
+        for(int j = 7; j >= 0; --j){
+            if(ch & (1 << j)) {
+                strcat(binary,"1");
+            } else {
+                strcat(binary,"0");
+            }
+        }
+    }
+    return binary;
+}
+
+char *inputString(FILE* fr, size_t size){
+//The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char)*size);//size is start size
+    if(!str)return str;
+    while(EOF!=(ch=fgetc(fr)) && ch != '\n'){
+        str[len++]=ch;
+        if(len==size){
+            str = realloc(str, sizeof(char)*(size+=16));
+            if(!str)return str;
+        }
+    }
+    str[len++]='\0';
+
+    return realloc(str, sizeof(char)*len);
+}
+
+int main(void){
+    FILE *fr = fopen("abc.txt", "r");
+    FILE *fw = fopen("abc.bin", "wb");
+    char *c;
+
+    c = inputString(fr, 10);
+    char *b = stringToBinary(c);
+    fwrite(b,strlen(b),1,fw);
+    printf("Characters in abc.txt succesfully converted into abc.bin.\n");
+
+    free(c);
+    fclose(fr);
+    fclose(fw);
+    return 0;
+}
